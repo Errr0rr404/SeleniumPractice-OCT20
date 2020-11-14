f1f0ae10904aed4634c78bc0fc4decea9162eb23
zann <naim_khsn@live.co.uk>
2020-11-14 11:43:27 -0500

    updated project

--- src/main/java/com/peoplentech/seleniumpractice/Selenium3.java
+++ src/main/java/com/peoplentech/seleniumpractice/Selenium3.java
@@ -1,18 +1,38 @@
 package com.peoplentech.seleniumpractice;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
 public class Selenium3 extends TestBase {
 
 
-    public static void main(String[] args) {
-        validateUserCanTypeOnSearchBar();
-    }
+    // xpath rules
+    //tagName[text()='______'] ---> text
+    //tagName[@attribute='_____'] --->id/class/aria-label etc
 
+
+    @Test
     public static void validateUserCanTypeOnSearchBar() {
         setupDriver();
         navigateToURL("https://www.ebay.com");
 
-        //
-        //
+        driver.findElement(By.id("gh-ac")).sendKeys("Java Books");
+        // driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("Java Books");
+
+        sleepFor(2);
+        closeDriver();
+    }
+
+
+    @Test
+    public static void validateUserCanTypeOnSearchBarAndClickSearchButton() {
+        setupDriver();
+        navigateToURL("https://www.ebay.com");
+
+        driver.findElement(By.id("gh-ac")).sendKeys("Java Books");
+        // driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("Java Books");
+
+        driver.findElement(By.xpath("//input[@id='gh-btn']")).click();
 
         sleepFor(2);
         closeDriver();
